dd93729d750bfb203b6823c70d2959e3c00b042a
FannyGao <593938430@qq.com>
2018-05-21 18:28:47 +0800

    'shoes'

---
+++ src/components/login/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+    input: ''
+    }
+  },
+  created(){
+  this.$http.get('/api/test').then(res=>{
+      console.log(res)
+  })
+  }
+}
+</script>
+
+<style>
+</style>
